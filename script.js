bfb94011a1eab0e2d98c155ead1268d79a24c2dd
leilahuang0714 <chihyunh@andrew.cmu.edu>
2021-04-10 20:36:36 -0400

    test

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ const materialTag = document.querySelector('.material')
 const problemTag = document.querySelector('.problem')
 
 send.addEventListener('click', ()=>{
-    const msgEl = document.createElement('li')
     let message =msg.value
-    msgEl.innerText = message
-    msgContent.appendChild(msgEl)
+    if(message === 'no'){
+        alert('no')
+        const msgEl = document.createElement('li')
+        msgEl.innerText = message
+        msgContent.appendChild(msgEl)
+    } else {
+        const msgEl = document.createElement('li')
+        msgEl.innerText = message
+        msgContent.appendChild(msgEl)
+    }
 })
 
 
